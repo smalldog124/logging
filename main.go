c67dbd2495c009f6611484ce704422cc13863185
smalldog124 <panumars124@gmail.com>
2018-11-10 23:36:17 +0700

    added:ร้อยการทำงานของการเก็บ log ที่ไฟล์ main.go

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"log"
 	"logging/route"
+	"os"
 )
 
 func main() {
-	route := route.NewRoute()
+	file, err := os.OpenFile("logger.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalln("Failed to open log file", err)
+	}
+	defer file.Close()
+	logging := log.New(file, "INFO:", log.Ldate|log.Ltime|log.Lshortfile)
+	route := route.NewRoute(logging)
 	route.Run(":3000")
 }
